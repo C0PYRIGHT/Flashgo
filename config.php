4de027e653ede31de3fe96b1debdd59e30da8008
COPYRIGHT <47739907+C0PYRIGHT@users.noreply.github.com>
2019-03-20 05:37:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="FaKe~SyStEm";
+
+//user_gaid wajib isi
+$user_gaid= "44e65bbc-150d-460f-9b7c-16508a10fcac";
+//user_token wajib di isi //
+$user_token="AbzC5BrVoEr3JNFn";
+
+//user_id wajib di isi //
+$user_id="Facebook_256687128572413";
+
+// invite_code wajib di isi //
+$invite_code="F1At7UWViP";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18yNTY2ODcxMjg1NzI0MTMiLCJleHAiOiIxNTU1MTQ3Nzc1ODA2In0.49roewne_coM8t9WU7PPN4riWgpKSC0t9k-pyKhtkAM";
+
+
+
+?>
